fda152063472ff1efb22e7829f376d4064565284
Avneet Kaur <116372464+AvneetRiya@users.noreply.github.com>
2022-10-21 14:13:08 -0400

    Day1Assignment1
    Day1Assignment1

---
+++ Student.java
@@ -0,0 +1,24 @@
+package Sep22.day1.IntroJava;
+
+public class Student {
+	//method for 
+	public void studentDetail()
+	{
+		int roll_no =1;
+		String name ="Avneet Kaur";
+		System.out.println("Roll No. : "+roll_no);
+		System.out.println("Name : "+name);
+	}
+	
+	
+	public static void main(String[] args) {
+		
+		
+		// TODO Auto-generated method stub
+		
+	      Student Avneet= new Student();
+Avneet.studentDetail();
+
+	}
+
+}
