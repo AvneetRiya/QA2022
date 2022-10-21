4b5c8080fbaf437fe7573753d0a42f1c557fcc85
Avneet Kaur <116372464+AvneetRiya@users.noreply.github.com>
2022-10-21 14:19:03 -0400

    Day2Assignment1
    Day2Assignment1

---
+++ Shirt.java
@@ -0,0 +1,29 @@
+package Sep22.day1.IntroJava;
+
+public class Shirt {
+
+	
+	//Main
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		String ShirtColor = "Green";
+		
+		if (ShirtColor == "Red")
+			System.out.println(ShirtColor);
+		
+		else if (ShirtColor == "Green")
+			System.out.println(ShirtColor);
+		
+		else if (ShirtColor == "White")
+			System.out.println(ShirtColor);
+		
+		else if (ShirtColor == "Yellow")
+			System.out.println(ShirtColor);
+		
+		else System.out.println("unknown");
+		
+		
+	}
+
+}
